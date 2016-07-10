273d3d636d3ec0868c5e2f34203e1d63ab13bca9
Liuhao-bupt <Liuhao_bupt@163.com>
2016-07-10 18:50:56 +0800

    Update Exercise 1.py

--- Exercise 1.py
+++ Exercise 1.py
@@ -6,3 +6,7 @@ print "This is fun."
 print "Yay! printing."
 print "I'd much rather you 'not'."
 print 'I "said" do not touch this.'
+#单引号‘’和双引号“”都表示字符串
+#单引号‘’表示的字符串可以包含双引号“”但不能包含单引号。
+#双引号“”表示的字符串可以包含单引号‘’但不能包含双引号。
+#如果无法显示中文请在开头加上# -*- coding: utf-8 -*-。
