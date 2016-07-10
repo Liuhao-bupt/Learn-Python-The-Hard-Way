62e3bd38efcb583fbf0b3a64b70f92748dad23db
Liuhao-bupt <Liuhao_bupt@163.com>
2016-07-10 18:48:03 +0800

    Create Exercise 1.py

---
+++ Exercise 1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+print "Hello World!"
+print "Hello Again"
+print "中文"
+print "This is fun."
+print "Yay! printing."
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
