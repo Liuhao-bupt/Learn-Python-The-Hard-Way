0d9ebf715582463a1eaa4648b249d4afc1d16997
Liuhao-bupt <Liuhao_bupt@163.com>
2016-07-12 13:48:37 +0800

    Create Exercise 9.py

---
+++ Exercise 9.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+print "Here are the days: ", days
+print "Here are the months: ", months
+print """
+There's something going on here.
+with the three double-quotes.
+we'll be able to type as much as we like."""
+print "\ngood"
+print """good good study " cau nnn"   ''''   \n \ntai\tjjj"""
+#关于“”“   ”“”，其中间的内容，对于引号和双引号将会毫无保留的输出来，但对于一些不可见的\n,\t将会输出
+成相应的换行和空格。
