505519730060bd95e76ca5d8f70c3c80048084a2
Liuhao-bupt <Liuhao_bupt@163.com>
2016-07-15 19:47:49 +0800

    Create Exercise 29.py

---
+++ Exercise 29.py
@@ -0,0 +1,13 @@
+people = 20
+cats = 30
+dogs = 15
+if people < cats:
+    print "Too many cats"
+    
+if people > cats:
+    print "Not too many cats"
+    
+if people < dogs:
+    print "The world is drooled on!"
+
+#if语句后要加上:
