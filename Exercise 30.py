94355a5050cd3845562079765eaff376154f0d3a
Liuhao-bupt <Liuhao_bupt@163.com>
2016-07-15 19:50:17 +0800

    Create Exercise 30.py

---
+++ Exercise 30.py
@@ -0,0 +1,13 @@
+people = 20
+cars = 40
+if cars > people:
+    print "We should take the cars."
+elif cars < people:
+    print "We should not take the cars."
+else:
+    print "We can't decide."
+
+# if:
+  elif:
+  else:
+这样的结构
